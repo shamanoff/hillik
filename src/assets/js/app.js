201d81315de4ed27a51592b6e7068f29da74939e
shamanoff <shamanoff@gmail.com>
2017-03-09 16:49:40 +0200

    work

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,14 @@
-
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 0,
+        loop: true
+    });
+});
 
 $(document).foundation();
