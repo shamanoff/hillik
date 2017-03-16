9be7484176f679b0dc315081c1cb60ca3e6e29e7
shamanoff <shamanoff@gmail.com>
2017-03-16 16:48:52 +0200

    work

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,16 @@
+$(function(){
+
+    var url = window.location.pathname,
+        urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+    // now grab every link from the navigation
+    $('#nav li a').each(function(){
+        // and test its normalized href against the url pathname regexp
+        if(urlRegExp.test(this.href.replace(/\/$/,''))){
+            $(this).addClass('active');
+        }
+    });
+
+});
 $(document).ready(function () {
     //initialize swiper when document ready
     var swiper = new Swiper('.swiper-container', {
@@ -12,3 +25,5 @@ $(document).ready(function () {
 });
 
 $(document).foundation();
+
+
